00ff22ab4e36b7d6a52746f7c29c2d3613ee31db
JepriAlber <jeprialber@gmail.com>
2021-08-19 20:17:50 +0700

    membuat hapus data pengguna app

---
+++ pengguna/delete.php
@@ -0,0 +1,16 @@
+<?php  
+	if (isset($_GET['id'])) {
+		require_once "../config/config.php";
+		$id 		= $_GET['id'];
+		$result 	= mysqli_query($con,"DELETE FROM user WHERE pengguna_id =$id")or die (mysqli_error($con));
+			if ($result == TRUE) {
+				header('location:data.php');
+				$_SESSION['pesan']		= "Data berhasil dihapus!";
+				$_SESSION['kondisi']	= "alert-success";
+			}else{
+				header('location:data.php');
+				$_SESSION['pesan']		= "Data gagal dihapus!";
+				$_SESSION['kondisi']	= "alert-danger";
+			}
+	}
+?>
